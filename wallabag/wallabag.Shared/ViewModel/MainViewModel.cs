57f0af3ecd1834634e829361c48e67f8545fd6de
Julian Oster <github@jlnostr.de>
2014-11-23 17:28:35 +0100

    Remove the design data 'cause they are not working right now.

--- wallabag/wallabag.Shared/ViewModel/MainViewModel.cs
+++ wallabag/wallabag.Shared/ViewModel/MainViewModel.cs
@@ -187,21 +187,6 @@ namespace wallabag.ViewModel
             refreshCommand = new RelayCommand(async () => await refresh(), () => IsRunning);
             addLinkCommand = new RelayCommand(() => addLink(), () => IsRunning);
             openSettingsCommand = new RelayCommand(() => openSettings(), () => IsRunning);
-
-            if (IsInDesignMode)
-            {
-                for (int i = 0; i < 10; i++)
-                {
-                    Article a = new Article();
-                    a.Content = "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, ut porta lorem lacinia consectetur. Donec ut libero sed arcu vehicula ultricies a non tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut gravida lorem. Ut turpis felis, pulvinar a semper sed, adipiscing id dolor. Pellentesque auctor nisi id magna consequat sagittis. Curabitur dapibus enim sit amet elit pharetra tincidunt feugiat nisl imperdiet. Ut convallis libero in urna ultrices accumsan. Donec sed odio eros. Donec viverra mi quis quam pulvinar at malesuada arcu rhoncus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In rutrum accumsan ultricies. Mauris vitae nisi at sem facilisis semper ac in est.</p>";
-                    a.Title = "Article No. " + i.ToString();
-                    a.Url = new Uri("http://wallabag.org/");
-
-                    unreadItems.Add(new ArticleViewModel(a));
-                    favouriteItems.Add(new ArticleViewModel(a));
-                    archivedItems.Add(new ArticleViewModel(a));
-                }
-            }
         }
     }
 }
