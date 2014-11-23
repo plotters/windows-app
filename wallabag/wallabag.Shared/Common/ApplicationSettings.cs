06f81b1ccffc07a8bf0bd17cb806f42215d4dc46
Julian Oster <github@jlnostr.de>
2014-11-23 19:26:18 +0100

    Updated ApplicationSettings - is working now!

--- wallabag/wallabag.Shared/Common/ApplicationSettings.cs
+++ wallabag/wallabag.Shared/Common/ApplicationSettings.cs
@@ -1,13 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Windows.Storage;
 
 namespace wallabag.Common
 {
     public static class ApplicationSettings
     {
-        public static void SetSetting<T>(string key, T value, bool roaming = false, bool save = false)
+        public static void SetSetting<T>(string key, T value, bool roaming = false)
         {
+            if (roaming)
+            {
+                if (!ApplicationData.Current.RoamingSettings.Values.ContainsKey(key))
+                    ApplicationData.Current.RoamingSettings.Values[key] = value;
+            }
+            else
+            {
+                if (!ApplicationData.Current.LocalSettings.Values.ContainsKey(key))
+                    ApplicationData.Current.LocalSettings.Values[key] = value;
+            }
         }
 
         public static T GetSetting<T>(string key)
@@ -17,7 +28,17 @@ namespace wallabag.Common
 
         public static T GetSetting<T>(string key, T defaultValue, bool roaming = false)
         {
-            return default(T);
+            if (roaming)
+            {
+                if (ApplicationData.Current.RoamingSettings.Values.ContainsKey(key))
+                    return (T)ApplicationData.Current.RoamingSettings.Values[key];
+            }
+            else
+            {
+                if (ApplicationData.Current.LocalSettings.Values.ContainsKey(key))
+                    return (T)ApplicationData.Current.LocalSettings.Values[key];
+            }
+            return (T)defaultValue;
         }
     }
 }
