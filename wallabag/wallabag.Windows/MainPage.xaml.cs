e020718c73810279a2ea2fbda83a990a8c5a1a84
Julian Oster <github@jlnostr.de>
2014-11-23 15:06:02 +0100

    Let's start with the Windows app.

--- wallabag/wallabag.Windows/MainPage.xaml.cs
+++ wallabag/wallabag.Windows/MainPage.xaml.cs
@@ -12,6 +12,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using wallabag.Common;
+using wallabag.ViewModels;
 
 // Die Elementvorlage "Leere Seite" ist unter http://go.microsoft.com/fwlink/?LinkId=234238 dokumentiert.
 
@@ -22,9 +24,39 @@ namespace wallabag
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private ObservableDictionary _defaultViewModel = new ObservableDictionary();
+        public ObservableDictionary DefaultViewModel
+        {
+            get { return this._defaultViewModel; }
+        }
+
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (e.PageState != null && e.PageState.ContainsKey("VM"))
+            {
+                DefaultViewModel["VM"] = (MainViewModel)e.PageState["VM"];
+
+                if (e.PageState.ContainsKey("selectedPivotItem"))
+                    mainPivot.SelectedIndex = Convert.ToInt32(e.PageState["selectedPivotItem"]);
+            }
+            else
+            {
+                DefaultViewModel["VM"] = new MainViewModel();
+            }
+
+            DefaultViewModel["unreadItems"] = ((MainViewModel)DefaultViewModel["VM"]).unreadItems;
+            DefaultViewModel["favouriteItems"] = ((MainViewModel)DefaultViewModel["VM"]).favouriteItems;
+            DefaultViewModel["archivedItems"] = ((MainViewModel)DefaultViewModel["VM"]).archivedItems;
+            DefaultViewModel["refreshCommand"] = ((MainViewModel)DefaultViewModel["VM"]).refreshCommand;
+            DefaultViewModel["addLinkCommand"] = ((MainViewModel)DefaultViewModel["VM"]).addLinkCommand;
+            DefaultViewModel["openSettingsCommand"] = ((MainViewModel)DefaultViewModel["VM"]).openSettingsCommand;
+            DefaultViewModel["AddLinkButtonVisibility"] = ((MainViewModel)DefaultViewModel["VM"]).AddLinkButtonVisibility;
+            DefaultViewModel["currentItems"] = ((MainViewModel)DefaultViewModel["VM"]).unreadItems;
         }
     }
 }
