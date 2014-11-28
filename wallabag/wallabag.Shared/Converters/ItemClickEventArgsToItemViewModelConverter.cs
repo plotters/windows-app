34a6bc28d2d7d78eaa21179eedbc12f88a34e7e3
Julian Oster <github@jlnostr.de>
2014-11-28 19:32:05 +0100

    Make use of NavigationService.

---
+++ wallabag/wallabag.Shared/Converters/ItemClickEventArgsToItemViewModelConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+
+namespace wallabag.Converters
+{
+    class ItemClickEventArgsToItemViewModelConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return (value as ItemClickEventArgs).ClickedItem;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
