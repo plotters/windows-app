9f906e249b3af8cb77484d739fefec5ac06ccd19
Julian Oster <github@jlnostr.de>
2014-11-28 17:22:47 +0100

    Prepare ItemPage for MVVM light.

--- wallabag/wallabag.WindowsPhone/Views/ItemPage.xaml.cs
+++ wallabag/wallabag.WindowsPhone/Views/ItemPage.xaml.cs
@@ -16,105 +16,21 @@ namespace wallabag.Views
     /// </summary>
     public sealed partial class ItemPage : Page
     {
-        private NavigationHelper navigationHelper;
-
-        private ObservableDictionary _defaultViewModel = new ObservableDictionary();
-        public ObservableDictionary defaultViewModel
-        {
-            get { return this._defaultViewModel; }
-        }
-
         public ItemPage()
         {
             this.InitializeComponent();
-
-            this.navigationHelper = new NavigationHelper(this);
-            this.navigationHelper.LoadState += this.NavigationHelper_LoadState;
-            this.navigationHelper.SaveState += this.NavigationHelper_SaveState;
-
-            this.shareCommand = new RelayCommand(() => share());
-            this.defaultViewModel["shareCommand"] = shareCommand;
-        }
-
-        /// <summary>
-        /// Ruft den <see cref="NavigationHelper"/> ab, der mit dieser <see cref="Page"/> verknüpft ist.
-        /// </summary>
-        public NavigationHelper NavigationHelper
-        {
-            get { return this.navigationHelper; }
-        }
-
-        /// <summary>
-        /// Füllt die Seite mit Inhalt auf, der bei der Navigation übergeben wird.  Gespeicherte Zustände werden ebenfalls
-        /// bereitgestellt, wenn eine Seite aus einer vorherigen Sitzung neu erstellt wird.
-        /// </summary>
-        /// <param name="sender">
-        /// Die Quelle des Ereignisses, normalerweise <see cref="NavigationHelper"/>
-        /// </param>
-        /// <param name="e">Ereignisdaten, die die Navigationsparameter bereitstellen, die an
-        /// <see cref="Frame.Navigate(Type, Object)"/> als diese Seite ursprünglich angefordert wurde und
-        /// ein Wörterbuch des Zustands, der von dieser Seite während einer früheren
-        /// beibehalten wurde.  Der Zustand ist beim ersten Aufrufen einer Seite NULL.</param>
-        private void NavigationHelper_LoadState(object sender, LoadStateEventArgs e)
-        {
-            if (e.NavigationParameter != null)
-            {
-                this._defaultViewModel["Item"] = (ArticleViewModel)e.NavigationParameter;
-                this._defaultViewModel["Title"] = ((ArticleViewModel)e.NavigationParameter).Title;
-                this.webView.NavigateToString(((ArticleViewModel)e.NavigationParameter).Content);
-            }
-        }
-
-        /// <summary>
-        /// Behält den dieser Seite zugeordneten Zustand bei, wenn die Anwendung angehalten oder
-        /// die Seite im Navigationscache verworfen wird.  Die Werte müssen den Serialisierungsanforderungen
-        /// von <see cref="SuspensionManager.SessionState"/> entsprechen.
-        /// </summary>
-        /// <param name="sender">Die Quelle des Ereignisses, normalerweise <see cref="NavigationHelper"/></param>
-        /// <param name="e">Ereignisdaten, die ein leeres Wörterbuch zum Auffüllen bereitstellen
-        /// serialisierbarer Zustand.</param>
-        private void NavigationHelper_SaveState(object sender, SaveStateEventArgs e)
-        {
-        }
-
-        #region NavigationHelper-Registrierung
-
-        /// <summary>
-        /// Die in diesem Abschnitt bereitgestellten Methoden werden einfach verwendet, um
-        /// damit NavigationHelper auf die Navigationsmethoden der Seite reagieren kann.
-        /// <para>
-        /// Platzieren Sie seitenspezifische Logik in Ereignishandlern für  
-        /// <see cref="NavigationHelper.LoadState"/>
-        /// und <see cref="NavigationHelper.SaveState"/>.
-        /// Der Navigationsparameter ist in der LoadState-Methode verfügbar 
-        /// zusätzlich zum Seitenzustand, der während einer früheren Sitzung beibehalten wurde.
-        /// </para>
-        /// </summary>
-        /// <param name="e">Stellt Daten für Navigationsmethoden und -ereignisse bereit.
-        /// Handler, bei denen die Navigationsanforderung nicht abgebrochen werden kann.</param>
-        protected override void OnNavigatedTo(NavigationEventArgs e)
-        {
-            this.navigationHelper.OnNavigatedTo(e);
-            DataTransferManager dataTransferManager = DataTransferManager.GetForCurrentView();
-            dataTransferManager.DataRequested += dataTransferManager_DataRequested;
         }
 
         void dataTransferManager_DataRequested(DataTransferManager sender, DataRequestedEventArgs args)
         {
             DataRequest request = args.Request;
-            request.Data.Properties.Title = this.defaultViewModel["Title"].ToString();
-            request.Data.SetWebLink(((ArticleViewModel)this.defaultViewModel["Item"]).Url);
+            //request.Data.Properties.Title = this.defaultViewModel["Title"].ToString();
+            //request.Data.SetWebLink(((ArticleViewModel)this.defaultViewModel["Item"]).Url);
         }
-
-        protected override void OnNavigatedFrom(NavigationEventArgs e)
-        {
-            this.navigationHelper.OnNavigatedFrom(e);
-        }
-
-        #endregion
 
         private async void webView_NavigationStarting(WebView sender, WebViewNavigationStartingEventArgs args)
         {
+            // Opens links in the Internet Explorer and not in the webView.
             if (args.Uri != null && args.Uri.AbsoluteUri.StartsWith("http"))
             {
                 args.Cancel = true;
@@ -122,11 +38,7 @@ namespace wallabag.Views
             }
         }
 
-        public RelayCommand shareCommand { get; private set; }
-        private void share()
-        {
-            DataTransferManager.ShowShareUI();
-        }
+        
 
     }
 }
