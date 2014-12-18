5cb881875a7098401f0306039d34cfa3e8f6b717
Julian Oster <github@jlnostr.de>
2014-12-18 20:45:56 +0100

    Add setting flyouts to Windows app.

--- wallabag/wallabag.Windows/MainPage.xaml.cs
+++ wallabag/wallabag.Windows/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Windows.UI.Xaml.Controls;
 using wallabag.Common;
+using wallabag.Views;
 
 namespace wallabag
 {
@@ -8,6 +9,27 @@ namespace wallabag
         public MainPage()
         {
             this.InitializeComponent();
+            Windows.UI.ApplicationSettings.SettingsPane.GetForCurrentView().CommandsRequested += MainPage_CommandsRequested;
+        }
+
+        void MainPage_CommandsRequested(Windows.UI.ApplicationSettings.SettingsPane sender, Windows.UI.ApplicationSettings.SettingsPaneCommandsRequestedEventArgs args)
+        {
+            Windows.UI.ApplicationSettings.SettingsCommand generalSettings =
+                new Windows.UI.ApplicationSettings.SettingsCommand("generalSettings", "General", (handler) =>
+            {
+                generalSettingsFlyout settingsFlyout = new generalSettingsFlyout();
+                settingsFlyout.Show();
+            });
+
+            Windows.UI.ApplicationSettings.SettingsCommand readingSettings =
+                new Windows.UI.ApplicationSettings.SettingsCommand("readingSettings", "Reading", (handler) =>
+            {
+                readingSettingsFlyout readingFlyout = new readingSettingsFlyout();
+                readingFlyout.Show();
+            });
+
+            args.Request.ApplicationCommands.Add(generalSettings);
+            args.Request.ApplicationCommands.Add(readingSettings);
         }
     }
 }
