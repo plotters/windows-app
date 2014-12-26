5163bae96db420e62e2fa06ab1ef0ee9c8fa293f
Julian Oster <github@jlnostr.de>
2014-12-26 18:04:25 +0100

    Add implementation for app resizing in basicPage.cs

--- wallabag/wallabag.Shared/Common/basicPage.cs
+++ wallabag/wallabag.Shared/Common/basicPage.cs
@@ -1,5 +1,7 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Core;
 
 namespace wallabag.Common
 {
@@ -10,10 +12,22 @@ namespace wallabag.Common
 
         public basicPage()
         {
+            this.Loaded += basicPage_Loaded;
+            this.Unloaded += basicPage_Unloaded;
             this.navigationHelper = new NavigationHelper(this);
             this.navigationHelper.LoadState += navigationHelper_LoadState;
             this.navigationHelper.SaveState += navigationHelper_SaveState;
         }
+
+        void basicPage_Loaded(object sender, RoutedEventArgs e) { Window.Current.SizeChanged += Window_SizeChanged; }
+        void basicPage_Unloaded(object sender, RoutedEventArgs e) { Window.Current.SizeChanged -= Window_SizeChanged; }
+
+        void Window_SizeChanged(object sender, WindowSizeChangedEventArgs e)
+        {
+            ChangedSize(e);
+        }
+
+        protected virtual void ChangedSize(WindowSizeChangedEventArgs e) { }
 
         void navigationHelper_SaveState(object sender, SaveStateEventArgs e)
         {
