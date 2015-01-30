c3b1d272159bfe02d72f8316b5ef44264c42f848
Julian Oster <github@jlnostr.de>
2015-01-30 15:45:08 +0100

    Woohoo! First implementation of the Refresh() method in MainViewModel.cs

--- wallabag/wallabag.Shared/ViewModel/MainViewModel.cs
+++ wallabag/wallabag.Shared/ViewModel/MainViewModel.cs
@@ -1,7 +1,10 @@
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.ObjectModel;
-using System.Threading.Tasks;
 using wallabag.Common;
 using Windows.Web.Http;
+using Windows.Web.Http.Headers;
 
 namespace wallabag.ViewModel
 {
@@ -10,11 +13,18 @@ namespace wallabag.ViewModel
         private ObservableCollection<ItemViewModel> _Items = new ObservableCollection<ItemViewModel>();
         public ObservableCollection<ItemViewModel> Items { get { return _Items; } }
 
-        private void Refresh()
+        private async void Refresh()
         {
-            HttpClient client = new HttpClient();
-            var test = client.GetStringAsync(new System.Uri("http://v2.wallabag.org/api/entries"));
-            var test2 = test.Status;
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Add(new HttpMediaTypeWithQualityHeaderValue("application/json"));
+            var response = await client.GetAsync(new Uri("http://wallabag-v2.jlnostr.de/api/entries"));
+            if (response.IsSuccessStatusCode)
+            {
+                foreach (Models.Item item in JsonConvert.DeserializeObject<ObservableCollection<Models.Item>>(await response.Content.ReadAsStringAsync()))
+                {
+                    Items.Add(new ItemViewModel(item));
+                }
+            }
         }
 
         public MainViewModel()
