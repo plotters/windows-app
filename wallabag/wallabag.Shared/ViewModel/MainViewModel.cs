708916e5a0c1415bf087ebe9528f3e3e61b40d79
Julian Oster <github@jlnostr.de>
2015-01-30 15:52:23 +0100

    Make use of using directives.

--- wallabag/wallabag.Shared/ViewModel/MainViewModel.cs
+++ wallabag/wallabag.Shared/ViewModel/MainViewModel.cs
@@ -13,23 +13,27 @@ namespace wallabag.ViewModel
         private ObservableCollection<ItemViewModel> _Items = new ObservableCollection<ItemViewModel>();
         public ObservableCollection<ItemViewModel> Items { get { return _Items; } }
 
+        private HttpClient client;
+
         private async void Refresh()
         {
-            var client = new HttpClient();
-            client.DefaultRequestHeaders.Accept.Add(new HttpMediaTypeWithQualityHeaderValue("application/json"));
-            var response = await client.GetAsync(new Uri("http://wallabag-v2.jlnostr.de/api/entries"));
-            if (response.IsSuccessStatusCode)
+            using (client)
             {
-                foreach (Models.Item item in JsonConvert.DeserializeObject<ObservableCollection<Models.Item>>(await response.Content.ReadAsStringAsync()))
+                var response = await client.GetAsync(new Uri("http://wallabag-v2.jlnostr.de/api/entries"));
+                if (response.IsSuccessStatusCode)
                 {
-                    Items.Add(new ItemViewModel(item));
+                    foreach (Models.Item item in JsonConvert.DeserializeObject<ObservableCollection<Models.Item>>(await response.Content.ReadAsStringAsync()))
+                    {
+                        Items.Add(new ItemViewModel(item));
+                    }
                 }
             }
         }
 
         public MainViewModel()
         {
-            Refresh();
+            client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Add(new HttpMediaTypeWithQualityHeaderValue("application/json"));
         }
     }
 }
