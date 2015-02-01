5f5350f17c1a7a0cb4c52a354727dd47e0aa15f4
Julian Oster <github@jlnostr.de>
2015-02-01 19:02:02 +0100

    Clean up ShareTargetPage.xaml.cs

--- wallabag/wallabag.Windows/Views/ShareTargetPage.xaml.cs
+++ wallabag/wallabag.Windows/Views/ShareTargetPage.xaml.cs
@@ -9,38 +9,20 @@ using Windows.UI.Xaml.Media.Imaging;
 using Windows.Web.Http;
 using Windows.Web.Http.Headers;
 
-// Die Elementvorlage "Freigabezielvertrag" ist unter http://go.microsoft.com/fwlink/?LinkId=234241 dokumentiert.
-
 namespace wallabag.Views
 {
-    /// <summary>
-    /// Über diese Seite können andere Anwendungen Inhalte durch diese Anwendung freigeben.
-    /// </summary>
     public sealed partial class ShareTargetPage : Page
     {
-        /// <summary>
-        /// Stellt einen Kanal zum Kommunizieren mit Windows über den Freigabevorgang bereit.
-        /// </summary>
         private Windows.ApplicationModel.DataTransfer.ShareTarget.ShareOperation _shareOperation;
         private ObservableDictionary defaultViewModel = new ObservableDictionary();
 
-        /// <summary>
-        /// Dies kann in ein stark typisiertes Anzeigemodell geändert werden.
-        /// </summary>
-        public ObservableDictionary DefaultViewModel
-        {
-            get { return this.defaultViewModel; }
-        }
+        public ObservableDictionary DefaultViewModel { get { return this.defaultViewModel; } }
 
         public ShareTargetPage()
         {
             this.InitializeComponent();
         }
 
-        /// <summary>
-        /// Wird aufgerufen, wenn eine andere Anwendung Inhalte durch diese Anwendung freigeben möchte.
-        /// </summary>
-        /// <param name="e">Aktivierungsdaten zum Koordinieren des Prozesses mit Windows.</param>
         public async void Activate(ShareTargetActivatedEventArgs e)
         {
             this._shareOperation = e.ShareOperation;
@@ -55,17 +37,12 @@ namespace wallabag.Views
             Window.Current.Activate();
         }
 
-        /// <summary>
-        /// Wird aufgerufen, wenn der Benutzer auf die Schaltfläche "Gemeinsam verwenden" klickt.
-        /// </summary>
-        /// <param name="sender">Instanz der Schaltfläche, die zum Initiieren der Freigabe verwendet wird.</param>
-        /// <param name="e">Ereignisdaten, die beschreiben, wie auf die Schaltfläche geklickt wurde.</param>
         private async void ShareButton_Click(object sender, RoutedEventArgs e)
         {
             this.DefaultViewModel["Sharing"] = true;
             this._shareOperation.ReportStarted();
 
-            HttpClient client = new HttpClient(); 
+            HttpClient client = new HttpClient();
             client.DefaultRequestHeaders.Accept.Add(new HttpMediaTypeWithQualityHeaderValue("application/json"));
             client.DefaultRequestHeaders.UserAgent.Add(new HttpProductInfoHeaderValue("wallabag for WinRT"));
 
