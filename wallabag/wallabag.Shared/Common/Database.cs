461be9001aafca9f94945b33890489db8b84fae8
Julian Oster <github@jlnostr.de>
2015-02-22 20:33:04 +0100

    Add Database.cs as future implementation.

---
+++ wallabag/wallabag.Shared/Common/Database.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace wallabag.Common
+{
+    class Database
+    {
+        public string Name { get; set; }
+               
+        public async Task Database(string databaseName)
+        {
+            this.Name = databaseName;
+            await ApplicationData.Current.LocalFolder.CreateFileAsync(Name + ".db");
+        }
+    }
+}
